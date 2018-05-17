2bfe42f41018cf4b05b921923197ff0707fccb58
Alfredo <dpd.12qwaszx>
2018-05-17 17:56:11 -0300

    Retirado import desnecessário.

--- src/main/java/br/uema/application/Application.java
+++ src/main/java/br/uema/application/Application.java
@@ -1,6 +1,5 @@
 package br.uema.application;
 
-import br.uema.application.config.DataSourceConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
